7dbafa31f18877f98909e587a3c983439c6971a1
kototok903 <kototok903@gmail.com>
2024-09-22 21:41:57 -0500

    Add WASD support

--- src/Game.java
+++ src/Game.java
@@ -255,15 +255,19 @@ public class Game extends JPanel implements ActionListener, KeyListener {
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()) {
             case KeyEvent.VK_UP:
+            case KeyEvent.VK_W:
                 up = true;
                 break;
             case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_S:
                 down = true;
                 break;
             case KeyEvent.VK_LEFT:
+            case KeyEvent.VK_A:
                 left = true;
                 break;
             case KeyEvent.VK_RIGHT:
+            case KeyEvent.VK_D:
                 right = true;
                 break;
             case KeyEvent.VK_SPACE:
@@ -280,15 +284,19 @@ public class Game extends JPanel implements ActionListener, KeyListener {
     public void keyReleased(KeyEvent e) {
         switch (e.getKeyCode()) {
             case KeyEvent.VK_UP:
+            case KeyEvent.VK_W:
                 up = false;
                 break;
             case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_S:
                 down = false;
                 break;
             case KeyEvent.VK_LEFT:
+            case KeyEvent.VK_A:
                 left = false;
                 break;
             case KeyEvent.VK_RIGHT:
+            case KeyEvent.VK_D:
                 right = false;
                 break;
         }
